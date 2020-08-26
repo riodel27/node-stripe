a07d9e9b4e29f2991d24cb83d65b5875ac5369e3
rpamaong <rpamaong@startechup.com>
2020-08-27 00:46:26 +0800

    initial commit

---
+++ server.js
@@ -0,0 +1,44 @@
+require("dotenv").config();
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const stripe = require("stripe")(process.env.STRIPE_KEY); // Add your Secret Key Here
+
+const app = express();
+
+// This will make our form data much more useful
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// This will set express to render our views folder, then to render the files as normal html
+app.set("view engine", "ejs");
+app.engine("html", require("ejs").renderFile);
+
+app.use(express.static(path.join(__dirname, "./views")));
+
+// Future Code Goes Here
+
+app.post("/charge", (req, res) => {
+  try {
+    stripe.customers
+      .create({
+        name: req.body.name,
+        email: req.body.email,
+        source: req.body.stripeToken,
+      })
+      .then((customer) => {
+        stripe.charges.create({
+          amount: req.body.amount * 100,
+          currency: "usd",
+          customer: customer.id,
+        });
+      })
+      .then(() => res.render("completed.html"))
+      .catch((err) => console.log(err));
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log("Server is running..."));
